09b54681a9d3650ddf8c71b6c0032808c30da3d0
Daniel Tan <dtch1997@users.noreply.github.com>
2022-11-04 15:10:53 +0000

    Add convenience script for replay and pretrained checkpoint

--- scripts/test_pointmaze.sh
+++ scripts/test_pointmaze.sh
@@ -1,1 +1,12 @@
-python -m rl.main_latent --env_name PointMaze-v1 --test_env_name PointMazeTest-v1 --n_workers 1 --n_epochs 10000 --batch_size 512 --grad_value_clipping -1.0 --seed 123 --run-mode eval-test --render
+python -m rl.main_latent \
+    --env_name PointMaze-v1 \
+    --test_env_name PointMazeTest-v1 \
+    --n_workers 1 \
+    --n_epochs 10000 \
+    --batch_size 512 \
+    --grad_value_clipping -1.0 \
+    --seed 123 \
+    --play \
+    --run-mode eval-test \
+    --render \
+    --resume_ckpt Nov-3-14-51-17
